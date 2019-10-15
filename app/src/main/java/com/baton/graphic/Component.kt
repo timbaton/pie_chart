b43a58db99cc2f269dcdf5ac18a6b494f6d69fae
Timur Badretdinov <tim_baton@mail.ru>
2019-10-15 11:50:37 +0300

    just commit

--- app/src/main/java/com/baton/graphic/Component.kt
+++
@@ -1,3 +0,0 @@
-package com.baton.graphic
-
-data class Component(val value: Double, var color: Int? = null)
--- app/src/main/java/com/baton/graphic/Segment.kt
+++
@@ -1,3 +0,0 @@
-package com.baton.graphic
-
-data class Segment(val value: Double, var color: Int? = null)
